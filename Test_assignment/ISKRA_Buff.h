43f27731bcc74481300645e37984b870dd1aa804
alacrad <alacrad11@gmail.com>
2020-02-08 14:09:05 +0300

    Changed allocated ptr memory from int8 to void

--- Test_assignment/ISKRA_Buff.h
+++ Test_assignment/ISKRA_Buff.h
@@ -4,5 +4,5 @@
 #define ISKRA_BUFF_H_
 	
 
-	__int8* ISKRABuffAssemble(__int8* MemoryForBuffer, __int8* TRK_No, __int8 Command, __int8* price, __int8* volume, __int8* status, __int8* error, __int8* code);
+	void* ISKRABuffAssemble(void* MemoryForBuffer, __int8* TRK_No, __int8 Command, __int8* price, __int8* volume, __int8* status, __int8* error, __int8* code);
 #endif // !ISKRA_BUFF_H_
--- Test_assignment/Main.c
+++ Test_assignment/Main.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <malloc.h>
 #include "ISKRA_Buff.h"
 
 
 int main() {
 	char mass[27];
+	void* buff = (__int8*)malloc(27);
 	__int8	TRK_No[2] = {'1','2'},
 			Command = '5',
 			price[6] = { 'p','r','i','c','e','!' },
@@ -12,10 +14,12 @@ int main() {
 			status[4] = {'s','t','u','s'},
 			error[2] = {'r','r'},
 			code[2] = {'c','d'};
-	__int8* df = ISKRABuffAssemble(mass, TRK_No, Command, price, volume, status, error, code);
+	__int8* df = ISKRABuffAssemble(buff, TRK_No, Command, price, volume, error, code);
 	int count = 0;
 	while (count < 27) {
-		printf("%c ", *(df + count));
+		printf("%c ", *((__int8*)buff + (int)count));
 		count++;
+		
+	}
 	return 0;
 }
